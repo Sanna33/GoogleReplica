2f764ca108c55712ed001b4157a24f8483097e7a
reececilon <reecethat@gmail.com>
2022-06-22 15:44:20 +0100

    add links

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 
 queries = [
-    {name: 'food', urllink: ''},
+    {name: 'food', urllink: 'https://en.wikipedia.org/wiki/Food'},
     {name: 'food', urllink: ''},
     {name: 'food', urllink: ''},
     {name: 'food', urllink: ''},
