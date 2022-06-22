01d486e97e7f192e95e503d76e59d00bf25dbf76
Sanna <naseemsanna33@gmail.com>
2022-06-22 15:49:00 +0100

    added 5 links

--- server/app.js
+++ server/app.js
@@ -8,9 +8,9 @@ queries = [
     {name: 'food', urllink: ''},
     {name: 'food', urllink: ''},
     {name: 'food', urllink: ''},
-    {name: 'food', urllink: ''},
-    {name: 'food', urllink: ''},
-    {name: 'food', urllink: ''},
-    {name: 'food', urllink: ''},
-    {name: 'food', urllink: ''}
+    {name: 'anime', urllink: 'https://en.wikipedia.org/wiki/Anime'},
+    {name: 'skydiving', urllink: 'https://ukskydiving.com/'},
+    {name: 'travel', urllink: 'https://www.tripadvisor.co.uk/'},
+    {name: 'tiktok', urllink: 'https://www.tiktok.com/'},
+    {name: 'spotify', urllink: 'https://open.spotify.com/'}
 ]
